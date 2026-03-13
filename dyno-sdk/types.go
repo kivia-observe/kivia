d5f0e9f26b5fe3f643463e8d67ea1aca9bcef63f
winner <eragbonikpeya@gmail.com>
2026-03-13 15:12:53 +0100

    chore(dyno sdk): Move log struct to types.go

--- dyno-sdk/types.go
+++ dyno-sdk/types.go
@@ -3,7 +3,24 @@ package dynosdk
 import (
 	"bytes"
 	"net/http"
+	"time"
 )
+
+type Log struct {
+
+	Id string `json:"id"`
+	
+	Path string `json:"path"`
+	
+	Status int `json:"status"`
+	
+	IPAddress string `json:"ip_address,omitempty"`
+
+	Timestamp time.Time `json:"timestamp"`
+	
+	Latency int `json:"latency"`
+}
+
 type Client struct {
 	BaseURL    string
 	ApiKey     string
