76aec306341c66030555f9f3e6b5d2f51954b1e8
winnerx0 <eragbonikpeya@gmail.com>
2026-03-20 11:13:00 +0100

    feat(dyno service): Add ErrSomethingWentWrong and ErrInvalidRevoke sentinel errors

--- dyno_service/internal/utils/errors.go
+++ dyno_service/internal/utils/errors.go
@@ -9,7 +9,10 @@ var (
 	ErrDuplicateKey = errors.New("Duplicate API key name for the project")
 	ErrApiKeyNotFound = errors.New("API key not found")
 	ErrInvalidApiKey = errors.New("Invalid API key")
+	ErrInvalidRovoke = errors.New("API key already revoked")
 
 	// project related errors
 	ErrProjectNotFound = errors.New("Project not found")
+	
+	ErrSomethingWentWrong = errors.New("Something went wrong")
 )
