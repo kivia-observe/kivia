bf681c9eadc467f783714491878367e96bce817a
winnerx0 <eragbonikpeya@gmail.com>
2026-04-04 11:35:31 +0100

    refactor(core): use default values for env vars instead of panicking

--- core/internal/config/config.go
+++ core/internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"os"
 	
 )
@@ -26,23 +25,23 @@ type Config struct {
 func Load() *Config {
 
 	return &Config{
-		DBUrl:                 getEnv("DATABASE_URL"),
-		Port:                  getEnv("PORT"),
-		JwtAccessTokenSecret:  getEnv("JWT_ACCESS_TOKEN_SECRET"),
-		JwtRefreshTokenSecret: getEnv("JWT_REFRESH_TOKEN_SECRET"),
-		RabbitMQConnectionUrl: getEnv("RABBITMQ_CONNECTION_URL"),
-		GoogleClientID:        getEnv("GOOGLE_CLIENT_ID"),
-		GoogleClientSecret:    getEnv("GOOGLE_CLIENT_SECRET"),
-		GoogleRedirectURL:     getEnv("GOOGLE_REDIRECT_URL"),
-		GoogleFrontendURL:     getEnv("GOOGLE_FRONTEND_URL"),
+		DBUrl:                 getEnv("DATABASE_URL", "postgresql://postgres:password@localhost:5000/dyno"),
+		Port:                  getEnv("PORT", "8081"),
+		JwtAccessTokenSecret:  getEnv("JWT_ACCESS_TOKEN_SECRET", "e057eb5d0604805f2809b58c892cc76ae7f6dffffee8e2d4f2807a4d8c86dfac"),
+		JwtRefreshTokenSecret: getEnv("JWT_REFRESH_TOKEN_SECRET", "74b8fa85124b7cd11dfe34e33fdd6063711eab4d950af94affd0b50fe4ab3d85"),
+		RabbitMQConnectionUrl: getEnv("RABBITMQ_CONNECTION_URL", "amqp://guest:guest@localhost:5672"),
+		GoogleClientID:        getEnv("GOOGLE_CLIENT_ID", "890866835336-cef30b36umgfmehp7bgf5mnff8riri7m.apps.googleusercontent.com"),
+		GoogleClientSecret:    getEnv("GOOGLE_CLIENT_SECRET", "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"),
+		GoogleRedirectURL:     getEnv("GOOGLE_REDIRECT_URL", "http://localhost:8080/auth/google/callback"),
+		GoogleFrontendURL:     getEnv("GOOGLE_FRONTEND_URL", "http://localhost:3000"),
 	}
 
 }
 
-func getEnv(key string) (value string) {
+func getEnv(key string, defaultValue string) (value string) {
 
 	if value = os.Getenv(key); value == "" {
-		panic(fmt.Sprintf("%s not in .env", key))
+		return defaultValue
 	}
 
 	return value
