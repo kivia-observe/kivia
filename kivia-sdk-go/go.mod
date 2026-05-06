3aca2c49b31df7ca71391a5faefa8c7e06cbca0d
winnerx0 <eragbonikpeya@gmail.com>
2026-05-06 12:36:02 +0100

    Remove SDKs from monorepo

--- kivia-sdk-go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/winnerx0/kivia-sdk-go
-
-go 1.25.0
--- kivia-sdk-js/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './types';
-export * from './client';
-export * from './fastify';
-export * from './hono';
-export * from './elysia';
